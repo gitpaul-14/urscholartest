3d607aa890f4a2ec0243692988e3f97ac5149e9b
John Paul Manalo <johnpauldeguzman.manalo29@gmail.com>
2025-01-16 14:35:34 +0800

    V1.3 modified sending

--- resources/js/Pages/Super_Admin/Scholarships/SendingAccess.vue
+++ resources/js/Pages/Super_Admin/Scholarships/SendingAccess.vue
@@ -2,6 +2,7 @@
     <Head title="Scholarships" />
     <AuthenticatedLayout>
         <div class="w-full h-full px-10 py-5 bg-[#F8F8FA] overflow-auto">
+            <form @submit.prevent="submitForm">
             <div class="w-full mx-auto p-3 rounded-xl text-white">
                 <div class="breadcrumbs text-sm text-gray-400 mb-5">
                     <ul>
@@ -67,43 +68,58 @@
                                             placeholder="Write additional informations here..."></textarea>
                                     </div>
                                 </div>
-                                <div class="w-full">
-                                    <h3 class="font-semibold text-gray-900 dark:text-white">Requirements</h3>
-                                    <ul class="w-full text-sm font-medium text-gray-900 bg-white dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                                        <div class="flex items-center mb-4 w-full">
-                                            <form @submit.prevent="addItem" class="flex items-center w-full">
-                                                <input v-model="newItem" type="text" placeholder="Enter an item"
-                                                    class="border border-gray-300 rounded-lg px-4 py-2 flex-grow" />
-                                                <button type="submit"
-                                                    class="bg-blue-500 text-white px-4 py-2 ml-2 rounded-lg hover:bg-blue-600">
-                                                    Add
-                                                </button>
-                                            </form>
+                                <div class="w-full flex flex-col space-y-5">
+                                    <div>
+                                        <h3 class="font-semibold text-gray-900 dark:text-white">Set Submission
+                                            Timeline</h3>
+                                        <div class="flex flex-row gap-3 w-full">
+                                            <div class="relative w-full">
+                                                <DatePicker class="w-full" v-model="form.appplication "
+                                                    placeholder="Submission Start" />
+                                            </div>
+                                            <div class="relative w-full">
+                                                <DatePicker class="w-full" v-model="form.deadline"
+                                                    placeholder="Submission Deadline" />
+                                            </div>
                                         </div>
-
-                                        <form @submit.prevent="removeItem">
-                                            <ul>
-                                                <li v-for="(item, index) in items" :key="index"
-                                                    class="flex items-center justify-between text-base bg-gray-100 px-4 py-2 mb-1 rounded-lg">
-                                                    <span>{{ item }}</span>
-                                                    <button @click="removeItem(index)"
-                                                        class="flex items-center text-red-500 hover:text-red-700">
-                                                        <span class="material-symbols-rounded text-red-600">
-                                                            delete
-                                                        </span>
+                                    </div>
+                                    <div class="w-full">
+                                        <h3 class="font-semibold text-gray-900 dark:text-white">Requirements</h3>
+                                        <ul class="w-full text-sm font-medium text-gray-900 bg-white dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                            <div class="flex items-center mb-4 w-full">
+                                                <form @submit.prevent="addItem" class="flex items-center w-full">
+                                                    <input v-model="newItem" type="text" placeholder="Enter an item"
+                                                        class="border border-gray-300 rounded-lg px-4 py-2 flex-grow" />
+                                                    <button type="submit"
+                                                        class="bg-blue-500 text-white px-4 py-2 ml-2 rounded-lg hover:bg-blue-600">
+                                                        Add
                                                     </button>
-                                                </li>
-                                            </ul>
-                                        </form>
-                                    </ul>
+                                                </form>
+                                            </div>
+
+                                            <form @submit.prevent="removeItem">
+                                                <div class="grid grid-cols-2 gap-4">
+                                                    <div v-for="(item, index) in items" :key="index"
+                                                        class="flex items-center justify-between text-base bg-gray-100 px-4 py-2 mb-1 rounded-lg">
+                                                        <span>{{ item }}</span>
+                                                        <button @click="removeItem(index)"
+                                                            class="flex items-center text-red-500 hover:text-red-700">
+                                                            <span class="material-symbols-rounded text-red-600">
+                                                                delete
+                                                            </span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-                
             </div>
+            </form> 
         </div>
     </AuthenticatedLayout>
 </template>
@@ -113,6 +129,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { ref, onMounted, computed } from 'vue';
 import { Head, useForm, Link } from '@inertiajs/vue3';
 import { Tooltip } from 'primevue';
+import { DatePicker } from 'primevue';
 
 const props = defineProps({
     scholars: {
@@ -127,16 +144,33 @@ const props = defineProps({
 
 const directives = {
     Tooltip,
+    DatePicker
 };
 
-const isCreating = ref(false);
-const isEditing = ref(false);
 const form = ref({
     id: null,
     name: '',
     description: '',
-    sponsor_id: ''
+    sponsor_id: '',
+    requirements: {},
 });
+
+// dynamic requirements
+const newItem = ref('');
+const items = ref([]);
+
+const addItem = () => {
+    if (newItem.value.trim() !== '') {
+        items.value.push(newItem.value.trim());
+        form.value.requirements = items.value;
+        newItem.value = '';
+    }
+};
+
+const removeItem = (index) => {
+    items.value = items.value.filter((_, i) => i !== index);
+};
+
 
 const getSponsorName = (sponsorId) => {
     const sponsor = props.sponsors.find(s => s.id === sponsorId);
