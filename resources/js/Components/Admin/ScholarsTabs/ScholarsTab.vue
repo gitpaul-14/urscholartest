897281f4df9007088f06f2c85daa07f7c1e3189d
Calvin-SoftEng <116782457+Calvin-SoftEng@users.noreply.github.com>
2025-01-18 23:18:49 +0800

    V1.3 Updated

--- resources/js/Components/Admin/ScholarsTabs/ScholarsTab.vue
+++ resources/js/Components/Admin/ScholarsTabs/ScholarsTab.vue
@@ -56,16 +56,16 @@
               <tr
                 class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  1
+                  {{ scholar.id }}
                 </th>
                 <td class="px-6 py-4">
-                  1
+                  {{ `${scholar.first_name} ${scholar.last_name}` }}
                 </td>
                 <td class="px-6 py-4">
-                  1
+                  {{ scholar.course }}
                 </td>
                 <td class="px-6 py-4">
-                  1
+                  {{ scholar.email }}
                 </td>
                 <td class="px-6 py-4">
                   <span
