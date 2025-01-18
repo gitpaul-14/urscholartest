df44d5b6b9d5eeebc91dd9165760893f0ec0c2f0
Calvin-SoftEng <116782457+Calvin-SoftEng@users.noreply.github.com>
2025-01-18 13:34:35 +0800

    V1.3 For redesign

--- resources/js/Components/Admin/ScholarsTabs/ScholarsTab.vue
+++ resources/js/Components/Admin/ScholarsTabs/ScholarsTab.vue
@@ -3,39 +3,12 @@
     <!-- line sections -->
     <div class="flex flex-row justify-between items-center mb-4">
       <div class="flex justify-between items-center">
-        <div>
-          <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-            Scholars</button>
-        </div>
-
-        <!-- <Link :href="route('requirementsTab.requirements')"> -->
-          <div>
-            <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-              Requirements</button>
-          </div>
-        <!-- </Link> -->
-
-        <div>
-          <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-            Monitoring</button>
-        </div>
-
-        <div>
-          <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-            Remind Submissions</button>
-        </div>
 
         <div>
           <button @click="toggleCreate" type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
             Add Scholars</button>
         </div>
 
-        <!-- <Link :href="`/scholarships/${scholarship.id}/send-access`"> -->
-          <div>
-            <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-              Send Access Details</button>
-          </div>
-        <!-- </Link> -->
       </div>
 
       <form class="w-3/12">   
--- resources/js/Pages/Super_Admin/Scholarships/Scholarship.vue
+++ resources/js/Pages/Super_Admin/Scholarships/Scholarship.vue
@@ -28,13 +28,32 @@
                     <h1 class="text-4xl font-poppins font-extrabold text-[darkblue] text-left">
                         <span>{{ scholarship.name }}</span> Scholars 2024-2025 ganyan
                     </h1>
-                </div>    
+                </div>
 
+                <div class="flex justify-between items-center">
+                    <div>
+                        <button type="button"
+                            class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                            Scholars</button>
+                    </div>
+
+                    <div>
+                        <button type="button"
+                            class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                            Requirements</button>
+                    </div>
+
+                    <div>
+                        <button type="button"
+                            class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                            Monitoring</button>
+                    </div>
+                </div>
                 <ScholarsTab />
                 <!-- <SlidingAddScholars /> -->
             </div>
             <!-- add here -->
-                
+
         </div>
     </AuthenticatedLayout>
 </template>
